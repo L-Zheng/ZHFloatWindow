ca08dcb7f742959418f9308dde155c7fe8dd3d61
L-Zheng <418736259@qq.com>
2020-03-28 15:25:09 +0800

    add prj

---
+++ ZHFloatWindow/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  ZHFloatWindow
+//
+//  Created by Zheng on 2020/3/28.
+//  Copyright © 2020 Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic,strong) UIWindow *window;
+
+@end
+
---
+++ ZHFloatWindow/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ZHFloatWindow
+//
+//  Created by Zheng on 2020/3/28.
+//  Copyright © 2020 Zheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
